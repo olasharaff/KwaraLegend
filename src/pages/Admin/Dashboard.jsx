505670c71eabb72e6112d3288358f25492a84bc6
AshraffDevv <sharafabdulrahman@icloud.com>
2024-05-31 10:30:46 +0100

    updated commited

--- src/pages/Admin/Dashboard.jsx
+++ src/pages/Admin/Dashboard.jsx
@@ -32,27 +32,27 @@ function Dashboard() {
               className="text-center font-bold text-2xl mb-2"
               style={{ color: mode === "dark" ? "white" : "black" }}
             >
-              Kamal Nayan Upadhyay
+             Kwara Legend
             </h1>
 
             <h2
               style={{ color: mode === "dark" ? "white" : "black" }}
               className="font-semibold"
             >
-              Software Developer
+              Blog profile
             </h2>
-            <h2
+            {/* <h2
               style={{ color: mode === "dark" ? "white" : "black" }}
               className="font-semibold"
             >
               knupadhyay784@gmail.com
-            </h2>
-            <h2
+            </h2> */}
+            {/* <h2
               style={{ color: mode === "dark" ? "white" : "black" }}
               className="font-semibold"
             >
               <span>Total Blog : </span> 15
-            </h2>
+            </h2> */}
             <div className=" flex gap-2 mt-2">
               <Link to={"/createblog"}>
                 <div className=" mb-2">
